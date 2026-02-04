ee63a085e4ea279099855a1e4bbacc6e605ae3a9
KeseniaL <lytt8358@mylaurier.ca>
2026-02-03 22:52:40 -0500

    Also added in some additional comments to offer more clarity in choices. second commit for this

--- ClientHandler.java
+++ ClientHandler.java
@@ -1,5 +1,5 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
 
 /*Setting up Thread-per-client. Responsible for the following:
     - Establish one thread per client
@@ -10,41 +10,43 @@ import java.io.*;
 // extend Thread class for Clienthnadler
 public class ClientHandler extends Thread{
     private Socket socket;
-    private BufferedReader in;
-    private PrintWriter out;
+    private BufferedReader in; //read text from client one at a time
+    private PrintWriter out; //sends response to client, make sure sent immediately
 
     public ClientHandler(Socket socket){
         this.socket = socket;
     }
 
-
+    //method will automatically execute 
     @Override
     public void run(){
-        try {
-            in = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+        //create input and output stream for this client.
+        try { 
+            in = new BufferedReader (new InputStreamReader(socket.getInputStream())); 
             out = new PrintWriter(socket.getOutputStream(), true);
 
-            sendHandshake(); //establishing handshake
+            //immediately send handshake info
+            sendHandshake(); 
 
             //Dealing with client requests, server here will read request as long as its not empty and generate a response
             String line;
             while ((line = in.readLine()) != null){
-                String response = ProtocolParser.handle(line);
-                out.println(response);
+                String response = ProtocolParser.handle(line); //this passes raw command to protocol parser
+                out.println(response); //send single response to client once command has gone through parser
 
-                //client has completed their request and wishes to disconnect
+                //client has completed their request and wishes to disconnect, close socket
                 if(line.equals("DISCONNECT")){
                     break;
                 }
             }
 
         } catch (Exception e) {
-            //If client abruptly/unexpectedly disconnects
+            //If client abruptly/unexpectedly disconnects, this makes sure server doesn't crash and burnnn
         } finally {
             try {socket.close();} catch (IOException ignored) {}
         }
     }
-
+    //Sends server configuration (rules and requirements) to client after connection
     private void sendHandshake(){
         out.println("BOARD " + BBoard.BOARD_WIDTH + " " + BBoard.BOARD_HEIGHT);
         out.println("NOTE " + BBoard.NOTE_WIDTH + " " + BBoard.NOTE_HEIGHT);
