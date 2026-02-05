39b2f1d2f2fa0e0bde7dc9e85972aa91370500d5
KeseniaL <lytt8358@mylaurier.ca>
2026-02-05 00:31:54 -0500

    spelling mistake fix

--- ServerConfig/tester.java
+++ ServerConfig/tester.java
@@ -3,13 +3,6 @@ import java.io.*;
 import java.net.*;
 
 
-/*
-class tester {
-    public static void main(String[] args) {
-        System.out.println("Hi, is this working?");
-    }
-}
-*/
 //quick testtt
 
 public class tester{
@@ -25,7 +18,7 @@ public class tester{
     System.out.println(in.readLine());
 
     //testing out a few commands
-    out.println("Post 1 1 white hello");
+    out.println("POST 1 1 white hello");
     System.out.println(in.readLine());
 
     out.println("PIN 1 1");
